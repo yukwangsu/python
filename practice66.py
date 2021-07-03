0338aad818dcf9a9d3f07b3d3bee48360a10e0b2
yukwangsu <86365727+yukwangsu@users.noreply.github.com>
2021-07-03 21:23:23 +0900

    practice66.py

---
+++ practice66.py
@@ -0,0 +1,18 @@
+class Vecter:
+    def __init__(self, value):
+        self.value = value
+        a = 0
+        for i in range(len(value)):
+            a += value[i]**2
+        self.amp = a**(1/2)
+    def __add__(self, object):
+        temp = [self.value[i] + object.value[i] for i in range(len(self.value))]
+        return Vecter(temp)
+    def __mul__(self, object):
+        s = 0
+        for i in range(len(self.value)):
+            s += self.value[i]*object.value[i] 
+        return s    
+    def __sub__(self, object):
+        temp = [self.value[i] - object.value[i] for i in range(len(self.value))]
+        return Vecter(temp)
